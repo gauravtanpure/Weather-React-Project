fa0684ac773fd3ffcae617f10bcc5d552c8de1ad
Gaurav Tanpure <gauravtanpure999@gmail.com>
2025-08-15 17:07:15 +0530

    Origin set

--- Weather-Backend/backend/main.py
+++ Weather-Backend/backend/main.py
@@ -14,6 +14,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://weather-app-stqa-fa.vercel.app/",
 ]
 app.add_middleware(
     CORSMiddleware,
